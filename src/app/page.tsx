ce4aeaf97c1383cd8a6cb8294329e3191c9a636c
Eric Zhang <superezzhang@gmail.com>
2025-01-29 22:55:42 -0500

    set up basic wikipeida text showing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
   return (
     <div className="">
-      <main className="">
-
+      <main className="flex flex-col">
+        <button className="bg-slate-200 text-xl p-5 hover:bg-slate-300 active:bg-slate-400">
+          Fetch Wikipeida Page
+        </button>
+        <p className="bg-slate-600 text-white text-center">{text}</p>
       </main>
     </div>
   );
