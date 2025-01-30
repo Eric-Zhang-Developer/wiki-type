c2b00490e1aeb0c7335c632e6b4614d417098b13
Eric Zhang <superezzhang@gmail.com>
2025-01-29 22:30:37 -0500

    get rid of unnecessary fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "WikiType",
-  description: "Wikipeida meets Type Racers",
-};
 
 export default function RootLayout({
   children,
