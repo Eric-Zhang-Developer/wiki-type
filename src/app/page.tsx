146f55a351b54fa1da1e6d4f7f203c4288b1eafa
Eric Zhang <superezzhang@gmail.com>
2025-01-29 23:10:51 -0500

    add skeleton fucntion for fetching pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-"use client"
+"use client";
 import { useState } from "react";
+import fetchPage from "@/utils/fetch-page";
 
 export default function Home() {
   const [text, setText] = useState("");
   return (
     <div className="">
       <main className="flex flex-col">
-        <button className="bg-slate-200 text-xl p-5 hover:bg-slate-300 active:bg-slate-400">
+        <button
+          className="bg-slate-200 text-xl p-5 hover:bg-slate-300 active:bg-slate-400"
+          onClick={() => setText(fetchPage())}
+        >
           Fetch Wikipeida Page
         </button>
         <p className="bg-slate-600 text-white text-center">{text}</p>
---
+++ src/utils/fetch-page.ts
@@ -0,0 +1,4 @@
+export default function fetchPage(): string {
+  console.log("fetching page");
+  return "Fetching page";
+}
