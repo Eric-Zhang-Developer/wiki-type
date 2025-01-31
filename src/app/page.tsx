0843819e75306fc449c98f645f19a04a7dd1f12c
Eric Zhang <superezzhang@gmail.com>
2025-01-30 20:58:16 -0500

    style page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,25 @@ import fetchPage from "@/utils/fetch-page";
 import cleanString from "@/utils/clean-text";
 
 export default function Home() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState("Placeholder Text");
 
   const handelFetch = async () => {
     const pageText = await fetchPage();
     setText(cleanString(pageText));
-  }
-  
+  };
   return (
     <div className="">
-      <main className="flex flex-col">
+      <main className="flex flex-col items-center gap-8">
         <button
-          className="bg-slate-200 text-xl p-5 hover:bg-slate-300 active:bg-slate-400"
+          className="transform bg-slate-800 text-white text-2xl mt-6
+          py-3 px-4 mr-4 rounded-lg shadow-md flex justify-center items-center gap-2
+          transition hover:shadow-xl hover:bg-emerald-500 hover:scale-105
+          w-3/4 lg:w-2/4 font-serif"
           onClick={() => handelFetch()}
         >
           Fetch Wikipeida Page
         </button>
-        <p className="bg-slate-600 text-white text-center">{text}</p>
+        <p className="bg-slate-900 text-slate-200 text-center p-4">{text}</p>
       </main>
     </div>
   );
