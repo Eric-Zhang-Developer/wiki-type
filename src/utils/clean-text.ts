0a1d0cdcc17a3c8d16450d8c788d3924fc77457a
Eric Zhang <superezzhang@gmail.com>
2025-02-06 13:59:15 -0500

    Fix clean text function not cleaning \ns

--- src/utils/clean-text.ts
+++ src/utils/clean-text.ts
@@ -1,6 +1,6 @@
 export default function cleanString(str : string): string {
   console.log("Uncleaned Text: ", str);
-  str = str.replace(/[\n=]/g, ' ');
-  str = str.replace(/[^\w\s,.'()]/g, '');
+  str = str.replace(/\\n/g, ' ');
+  str = str.replace(/[^\w\s,.'()-]/g, '');
   return str;
 }
